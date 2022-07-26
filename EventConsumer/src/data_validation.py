d4d6856ce331592299b5690d4434b1619884061a
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2022-07-26 13:21:11 +0300

    Updated file

--- EventConsumer/src/data_validation.py
+++ EventConsumer/src/data_validation.py
@@ -1,5 +1,7 @@
-from marshmallow import Schema, fields
+# Created by Aurimas A. Nausedas on 07/23/22.
+# Updated by Aurimas A. Nausedas on 07/24/22.
 
+from marshmallow import Schema, fields
 
 class Event(Schema):
     event_type = fields.Str(required=True)
