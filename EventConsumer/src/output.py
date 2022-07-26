d403004277c968a79cca1d51bca0a215c2ffdd07
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2022-07-26 14:35:08 +0300

    Updated output.py

--- EventConsumer/src/output.py
+++ EventConsumer/src/output.py
@@ -6,10 +6,9 @@ import json
 
 def persist_output(json_string, target_path) -> None:
     '''
-    Opening a target path of a string and
-    then converting it to a JSON object
-    i variable gives that in the terminal each JSON object is followed
-    by a new line character if JSON object is valid
+    Opening a target path of a string and then converting it to a JSON object
+    Variable i gives that in the terminal each JSON object is followed
+    by a new line character if JSON object is valid and i is bigger than 1
     :param json_string: String
     :param target_path: String
     :return: None
