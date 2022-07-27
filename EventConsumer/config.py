b8ee270127c8d4cf2b52149a9a24dc10f6f99bdd
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2022-07-27 11:22:36 +0300

    Updated config.py

--- EventConsumer/config.py
+++ EventConsumer/config.py
@@ -6,7 +6,7 @@ basedir = path.abspath(path.dirname(__file__))
 load_dotenv(path.join(basedir, '.env'))
 
 PORT = environ.get('PORT')
-TARGET_FILE_LOCATION = path.dirname(path.abspath(_file_)) + '/output/events.json'
+TARGET_FILE_LOCATION = path.dirname(path.abspath(__file__)) + '/output/events.json'
 
 if environ.get("TARGET_FILE_LOCATION") != '':
     TARGET_FILE_LOCATION = environ.get("TARGET_FILE_LOCATION")
